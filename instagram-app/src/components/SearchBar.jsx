23eda928f7a32e4b3234e265793590214623f489
Shola Ayeni <ayenisholah@yahoo.com>
2019-06-10 13:19:27 +0100

    SearchBar Component created

---
+++ instagram-app/src/components/SearchBar.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+// import PropTypes from 'prop-types';
+
+function SearchBar() {
+    return (
+        <>
+            <img src="./assets/ig_search_bar.png" alt="Search Bar"/>
+        </>
+    );
+}
+
+export default SearchBar;
