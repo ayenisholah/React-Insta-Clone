9993ce1605fefa97a1d7951c7c798e5c51d16063
Shola Ayeni <ayenisholah@yahoo.com>
2019-06-12 13:56:58 +0100

    loginPage and component created

---
+++ instagram-app/src/components/LoginPage/LoginPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const LoginPage = (props) => {
+  return (
+    <div className='page login'>
+      <button>Login</button>
+    </div>
+  )
+}
+
+export default LoginPage;
