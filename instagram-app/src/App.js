df3730c6d196c02f082c138a83e6787c3facbb57
Shola Ayeni <ayenisholah@yahoo.com>
2019-06-12 08:22:25 +0100

    Lifecycle methods added

--- instagram-app/src/App.js
+++ instagram-app/src/App.js
@@ -1,26 +1,42 @@
 import React, { Component } from 'react';
-import dummyData from './dummy-data';
 import SearchBar from './components/SearchBar';
+import dummyData from './dummy-data';
 import PostContainer from './components/PostContainer';
-import CommentSection from './components/CommentSection';
 
 class App extends Component {
-  constructor() {
-    super();
-    this.state= {
-      instagram: dummyData,
+  constructor(props) {
+    console.log('Constructing....')
+    super(props);
+    this.state={
+      instagram: [],
+      currentComment: null
     };
   }
 
+  componentDidMount() {
+    console.log('Component Mounted!') 
+
+    setTimeout(() => {
+      this.setState({ instagram: dummyData })
+    }, 1000);
+
+  }
+
+  likePhoto = id => {
+    
+  }
+
+  addNewComment = id => {
+    
+  }
+
   render() {
+    console.log('rendering...')
     return (
       <div className='App'>
         <SearchBar/>
         <PostContainer
-          instagram={this.state.instagram}
-        />
-        <CommentSection
-          instagram={this.state.instagram}
+        instagram={this.state.instagram}
         />
       </div>
     );
